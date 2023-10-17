3f6d7920f429e531db64ea50703ef1501be5165f
yyy <yyy@DESKTOP-3QFQ8JN>
2023-10-17 23:50:28 +0530

    Committed...!

---
+++ src/com/lab1/Flab.java
@@ -0,0 +1,15 @@
+package com.lab1;
+
+public class Flab {
+
+	public static void main(String[] args) {
+		
+		int n=10;
+		if(n==10)
+			System.out.println("Wow You are Amazing");
+		else
+			System.out.println("Oh! Shit...");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yyy
+ *
+ */
+module DevopsLab1 {
+}
